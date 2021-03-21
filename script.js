f28bfe9a53473341b4db97add75e37f28343a6de
Emily Wilbur <ewilbur94@gmail.com>
2021-03-20 23:29:44 -0400

    modified script.js//added password input

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
-// Assignment Code (converted strings to arrays to access characters more easily)
-var specialCharArray = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('');
-var numberArray = '1234567890'.split('');
-var lowerCaseArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var upperCaseArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+let passwordCharacters = [];
+let specialChars = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~'.split('');
+let lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+let upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+let numericChars = '1234567890'.split('');
+let newPassword = [];
 
-document.getElementById('generate').addEventListener('click'()) =>
-{
-    var generatedPassword = []; //array holding the generated password
-    
+
+
+// Write password to the #password input
+function writePassword() {
+    let passwordLength = prompt('How long would you like the password to be? (Must be greater than 8 and less than 128');
+    if(isNaN(passwordLength)){
+        alert('Password must be a number');
+        return;
+    }
+    if(passwordLength < 8)
+    {
+        alert('Password must be at least 8 characters long');
+        return;
+    }
+    if(passwordLength > 128)
+    {
+        alert('Password length must be no longer than 128 characters');
+        return;
+    }
+    if(confirm('Click OK to include numeric characters in your password'))
+    {
+       passwordCharacters.push(numericChars);
+    }
+    if(confirm('Click OK to include special characters in your password'))
+    {
+        passwordCharacters.push(specialChars);
+    }
+    if(confirm('Click OK to include upper case alphabetic characters'))
+    {
+        passwordCharacters.push(upperCaseChars);
+    }
+    if(confirm('Click OK to include lower case alphabetic characters'))
+    {
+        passwordCharacters.push(lowerCaseChars);
+    }   
+    if(passwordCharacters.length === 0)
+    {
+        alert('No characters were selected to be included in the password.');
+        return;
+    }
+    for(let x = 0; x < passwordLength; x++)
+    {
+        let newCharacter = generateCharacter();
+        newPassword.push(newCharacter);
+    }
+
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = newPassword.join('');
 }
+
+
+
+
+
+
+
